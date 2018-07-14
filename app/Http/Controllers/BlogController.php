d2fa1110570084f19778d74b489bca23851ff746
Unknown <andriwicaksonost@gmail.com>
2018-07-14 11:58:01 +0700

    Display only published posts Protected dates on app\Post routes/web.php add 'as'=>blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -11,7 +11,11 @@ class BlogController extends Controller
 	protected $limit = 5;
 
     public function index(){
-    	$posts = Post::with('author')->latestFirst()->paginate($this->limit);
+    	$posts = Post::with('author')
+    			->latestFirst()
+    			->published()
+    			->paginate($this->limit);
+    			
     	return view("blog.index", compact('posts'));
     }
 }
