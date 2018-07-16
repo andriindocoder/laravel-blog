482e94da6c311e7a2bc2f4b73b5d8976d4bf5ea9
Unknown <andriwicaksonost@gmail.com>
2018-07-16 13:22:55 +0700

    Custom 404 Error Page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 page-not-found">
+      	<br>
+        <h2>Page Not Found</h2>
+        <p>Halaman yang dicari tidak tersedia</p>
+      </div>
+    </div>
+  </div>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,6 +18,8 @@
     <!-- Custom styles for this template -->
     <link href="{{ asset('css/blog-home.css') }}" rel="stylesheet">
 
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
   </head>
 
   <body>
